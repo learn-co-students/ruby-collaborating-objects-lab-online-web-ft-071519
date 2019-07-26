11e2d71bb956e4b09bd3b06a5b23efd6242e5a90
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-26 12:17:54 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,23 @@
+require 'pry'
+
 class Song
+  
+
   attr_accessor :name, :artist
   def initialize(name)
     @name = name
+ 
   end
   
+
+  
   def self.new_by_filename(title)
-   
+   artist = Artist.new(title.split(" - ")[0])
+  
     song = Song.new(title.split(" - ")[1])
-    song.name = title.split(" - ")[1]
+    song.artist = artist
+  artist.add_song(song)
+    binding.pry
+return song
   end
 end
